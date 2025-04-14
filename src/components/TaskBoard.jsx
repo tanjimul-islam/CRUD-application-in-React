e67e131e7494d210506c02bbe5d283c9b25ad2df
Tanjimul Islam <tanjimul.islam@hotmail.com>
2025-04-15 00:03:53 +0600

    adding tasks

--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Search from "./Search";
+import AddTaskModal from "./task/AddTaskModal";
+import TaskAction from "./task/TaskAction";
 import TaskList from "./task/TaskList";
 
 const TaskBoard = () => {
@@ -13,11 +15,20 @@ const TaskBoard = () => {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showModal, setShowModal] = useState(false);
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+    setShowModal(false);
+  };
   return (
     <section className="mb-20" id="tasks">
+      {showModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         <Search />
-        <TaskList tasks={tasks} />
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskAction onAddTask={() => setShowModal(true)} />
+          <TaskList tasks={tasks} />
+        </div>
       </div>
     </section>
   );
--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,22 +1,10 @@
+import { FaStar } from "react-icons/fa";
+
 const Task = ({ task }) => {
   return (
     <tr className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
       <td>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-star"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="yellow"
-          fill="yellow"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-        </svg>
+        {task.isFavorite ? <FaStar color="yellow" /> : <FaStar color="gray" />}
       </td>
       <td>{task.title}</td>
       <td>
@@ -33,7 +21,7 @@ const Task = ({ task }) => {
           ))}
         </ul>
       </td>
-      <td className="text-center">High</td>
+      <td className="text-center">{task.priority}</td>
       <td>
         <div className="flex items-center justify-center space-x-3">
           <button className="text-red-500">Delete</button>
